37cc07727de40193268cfde7532bc4d37d9504c6
Ubuntu <ubuntu@ip-172-31-13-62.ec2.internal>
2024-09-14 14:59:56 +0000

    edits

--- accesslog.sh
+++ accesslog.sh
@@ -3,13 +3,10 @@
 # One liner
 # Read content of access.log
 # Extract only IP Addresses
-# Counts the occurrences of each IP address to identify how many requests came from each IP.(unique addresses)
-
-# awk '{print $9'} access.log
-
-grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' access.log | sort | uniq -c | sort -nr
+# Count the occurrences of each IP address to identify how many requests came from each IP.(unique addresses)
+# Sort the list by the number of requests in descending order
+# Save the sorted list to a file called ip_counts.txt
 
 
 
-
-
+grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' access.log | sort | uniq -c | sort -nr > ip_counts.txt 
